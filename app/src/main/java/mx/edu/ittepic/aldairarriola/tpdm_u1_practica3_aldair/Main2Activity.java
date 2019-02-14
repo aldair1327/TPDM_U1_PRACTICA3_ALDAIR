bf59d8d5ce18c4f2ab7fbc5b9ab86a2664aa5baa
aldair1327 <alarriolasa@ittepic.edu.mx>
2019-02-13 20:56:19 -0600

    uno

---
+++ app/src/main/java/mx/edu/ittepic/aldairarriola/tpdm_u1_practica3_aldair/Main2Activity.java
@@ -0,0 +1,16 @@
+package mx.edu.ittepic.aldairarriola.tpdm_u1_practica3_aldair;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        setTitle("Clubs");
+
+    }
+}
